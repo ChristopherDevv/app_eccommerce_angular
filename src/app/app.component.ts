c76407083aec8ba94065fbea8519af2a2bdec91d
christoper patino <christoper.patiho@gmail.com>
2024-03-05 17:51:24 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsService } from '@features/api/products.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app_todo';
+    private readonly productsSvc = inject(ProductsService);
+    products = this.productsSvc.products;
 }
